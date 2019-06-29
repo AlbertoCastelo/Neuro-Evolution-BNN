f3326b4eea61cbbb5b3f187b1618657f461092a1
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2019-06-29 18:37:23 +0200

    genome initialization

---
+++ neat/gene.py
@@ -0,0 +1,100 @@
+import numpy as np
+
+from neat.configuration import get_configuration
+
+NODE_TYPE = 'node'
+CONNECTION_TYPE = 'connection'
+
+
+class Gene:
+
+    def __init__(self, key, type):
+        self.key = key
+        self.type = type
+
+
+class ConnectionGene(Gene):
+
+    def __init__(self, key):
+        super().__init__(key=key, type=CONNECTION_TYPE)
+        self.key = key
+        self.enabled = True
+        self.config = get_configuration()
+        self.weight_configuration = WeightConfig()
+
+        self.weight_mean = None
+        self.weight_std = None
+
+    def random_initialization(self):
+        weight_mean_mean = self.weight_configuration.weight_mean_init_mean
+        weight_mean_std = self.weight_configuration.weight_mean_init_std
+
+        weight_mean = np.random.normal(loc=weight_mean_mean, scale=weight_mean_std)
+        weight_mean = np.clip(weight_mean,
+                              a_min=self.weight_configuration.weight_mean_min_value,
+                              a_max=self.weight_configuration.weight_mean_min_value)
+        self.weight_mean = weight_mean
+
+        weight_std_mean = self.weight_configuration.weight_std_init_mean
+        weight_std_std = self.weight_configuration.weight_std_init_std
+
+        weight_std = np.random.normal(loc=weight_std_mean, scale=weight_std_std)
+        weight_std = np.clip(weight_std,
+                             a_min=self.weight_configuration.weight_std_min_value,
+                             a_max=self.weight_configuration.weight_std_min_value)
+        self.weight_std = weight_std
+
+
+
+class NodeGene(Gene):
+
+    def __init__(self, key):
+        super().__init__(key=key, type=NODE_TYPE)
+        self.key = key
+        self.config = get_configuration()
+
+        self.activation = self.config.node_activation
+        self.aggregation = self.config.node_aggregation
+
+        self.bias_configuration = BiasConfig()
+        self.bias = None
+
+    def random_initialization(self):
+        mean = self.bias_configuration.bias_init_mean
+        std = self.bias_configuration.bias_init_std
+        bias = np.random.normal(loc=mean, scale=std)
+        bias = np.clip(bias,
+                       a_min=self.bias_configuration.bias_min_value,
+                       a_max=self.bias_configuration.bias_max_value)
+        self.bias = bias
+
+class BiasConfig:
+
+    def __init__(self):
+        config = get_configuration()
+        self.bias_init_mean = config.bias_init_mean
+        self.bias_init_std = config.bias_init_std
+        self.bias_max_value = config.bias_max_value
+        self.bias_min_value = config.bias_min_value
+        self.bias_mutate_power = config.bias_mutate_power
+        self.bias_mutate_rate = config.bias_mutate_rate
+        self.bias_replace_rate = config.bias_replace_rate
+
+
+class WeightConfig:
+    def __init__(self):
+        config = get_configuration()
+        self.weight_mean_init_mean = config.weight_init_mean
+        self.weight_mean_init_std = config.weight_init_std
+        self.weight_mean_max_value = config.weight_mean_max_value
+        self.weight_mean_min_value = config.weight_mean_min_value
+
+        self.weight_std_init_mean = config.weight_init_mean
+        self.weight_std_init_std = config.weight_init_std
+        self.weight_std_max_value = config.weight_std_max_value
+        self.weight_std_min_value = config.weight_std_min_value
+
+        weight_mutate_power = 0.5
+        weight_mutate_rate = 0.8
+        weight_replace_rate = 0.1
+
--- neat/genome.py
+++ neat/genome.py
@@ -1,9 +1,7 @@
 import uuid
 
 from neat.configuration import get_configuration
-
-NODE_TYPE = 'node'
-CONNECTION_TYPE = 'connection'
+from neat.gene import NodeGene, ConnectionGene
 
 
 class Genome:
@@ -18,44 +16,68 @@ class Genome:
 
         self.connection_genes = {}
         self.node_genes = {}
-
+        self.input_keys = None
         self.fitness = None
 
     def create_random_genome(self):
-        for key in self.get_output_node_keys():
-            self.node_genes[key] = NodeGene(key=key)
+        self._initialize_input_nodes()
+        self._initialize_output_nodes()
 
-    def get_output_node_keys(self):
+        # initialize hidden units
+        self._initialize_hidden_nodes()
+
+        # initialize connections
+        self._initialize_connections()
+
+    def _initialize_connections(self):
+        # initialize fully connected network with no recurrent connections
+        for input_node, output_node in self._compute_full_connections():
+            key = (input_node, output_node)
+            connection = ConnectionGene(key=key)
+            connection.random_initialization()
+            self.connection_genes[key] = connection
+
+    def _compute_full_connections(self):
+        connections = []
+        hidden_nodes = self._get_hidden_nodes()
+        if hidden_nodes:
+            for input_id in self.input_keys:
+                for h in hidden_nodes:
+                    connections.append((input_id, h))
+            for h in hidden_nodes:
+                for output_id in self._get_output_node_keys():
+                    connections.append((h, output_id))
+        for input_id in self.input_keys:
+            for output_id in self._get_output_node_keys():
+                connections.append((input_id, output_id))
+        # TODO: include recurrent connections if configured
+        return connections
+
+    def _initialize_hidden_nodes(self):
+        '''
+        hidden nodes have keys starting at n_outputs
+        '''
+        if self.genome_config.initial_hidden:
+            pass
+
+    def _initialize_output_nodes(self):
+        for key in self._get_output_node_keys():
+            node = NodeGene(key=key)
+            node.random_initialization()
+            self.node_genes[key] = node
+
+    def _get_output_node_keys(self):
         '''
         Output nodes have keys: 0, ..
         '''
         return range(self.n_output)
 
+    def _get_hidden_nodes(self):
+        return self.node_genes.keys()[self.n_output:]
 
-class Gene:
-
-    def __init__(self, key, type):
-        self.key = key
-        self.type = type
-
-
-class ConnectionGene(Gene):
-
-    def __init__(self, key):
-        super().__init__(key=key, type=CONNECTION_TYPE)
-        self.key = key
+    def _initialize_input_nodes(self):
+        # input nodes only contain keys (they cannot be evolved)
+        self.input_keys = list(range(-1, -self.n_input-1, -1))
 
 
-class NodeGene(Gene):
 
-    def __init__(self, key):
-        super().__init__(key=key, type=NODE_TYPE)
-        self.key = key
-        self.config = get_configuration()
-        
-        self.activation = self.config.node_activation
-        self.aggregation = self.config.node_aggregation
-        
-        self.bias_configuration = self.get_bias_configuration()
-
-    def get_bias_configuration(self):
