45823a730dda6fe424f1bf570ee1fe77dca64210
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2019-07-11 22:20:21 +0300

    create new test examples

--- tests/test_representation.py
+++ tests/test_representation.py
@@ -2,9 +2,8 @@ from unittest import TestCase
 
 from neat.gene import NodeGene
 from neat.genome import GenomeSample
-from neat.representation import get_nn_from_genome, Network
+from neat.representation import Network
 import torch
-import numpy as np
 
 
 class TestRepresentationFeedForwardWithoutHiddenLayers(TestCase):
@@ -31,6 +30,30 @@ class TestRepresentationFeedForwardWithoutHiddenLayers(TestCase):
         input = torch.tensor([1.0, -1.0])
         result = model.forward(input.data)
 
+        self.assertEqual(len(result), n_output)
+
+    def test_connection_is_not_specified_assumes_0(self):
+        node_0 = NodeGene(key=0)
+        node_0.random_initialization()
+        node_1 = NodeGene(key=1)
+        node_1.random_initialization()
+
+        node_genes = {0: node_0, 1: node_1}
+
+        connection_genes = {(-1, 0): 1.5,
+                            (-2, 0): 2.5,
+                            (-1, 1): -0.5}
+        n_output = 2
+        genome_sample = GenomeSample(key=None, n_input=2, n_output=n_output,
+                                     node_genes=node_genes,
+                                     connection_genes=connection_genes)
+
+        model = Network(genome=genome_sample)
+
+        input = torch.tensor([1.0, -1.0])
+        result = model.forward(input.data)
+        missing_weight = model.state_dict()['layer_0.weight'].data[1, 1]
+        self.assertEqual(0.0, missing_weight)
         self.assertEqual(len(result), n_output)
 
     def test_weights_are_correctly_set(self):
@@ -84,3 +107,53 @@ class TestRepresentationFeedForwardWithoutHiddenLayers(TestCase):
 
         self.assertEqual(len(result), n_output)
         self.assertTrue(torch.allclose(result, torch.tensor([0.7310, 0.5]), atol=1e-03))
+
+
+class TestRepresentationFeedForwardWithOneHiddenLayers(TestCase):
+
+    def test_network_structure(self):
+        genome_sample = generate_feedforward_with_one_hidden_unit()
+
+        model = Network(genome=genome_sample)
+
+        input = torch.tensor([1.0, -1.0])
+        result = model.forward(input.data)
+
+        self.assertEqual(len(result), genome_sample.n_output)
+
+        self.assertTrue(torch.allclose(result, torch.tensor([0.9820, 0.5]), atol=1e-02))
+
+
+def generate_feedforward_with_one_hidden_unit():
+    # output nodes
+    node_0 = NodeGene(key=0)
+    node_0.random_initialization()
+    node_0.bias = 1.0
+
+    node_1 = NodeGene(key=1)
+    node_1.random_initialization()
+    node_1.bias = 1.0
+
+    # hidden nodes
+    node_2 = NodeGene(key=2)
+    node_2.random_initialization()
+    node_2.bias = 1.0
+
+    node_3 = NodeGene(key=3)
+    node_3.random_initialization()
+    node_3.bias = -1.0
+
+    node_genes = {0: node_0, 1: node_1}
+
+    connection_genes = {(-1, 2): 1.5,
+                        (-2, 2): 2.5,
+                        (-1, 3): -0.5,
+                        (-2, 3): 0.0,
+                        (3, 0): -0.5,
+                        (2, 1): 0.0}
+
+    n_output = 2
+    genome_sample = GenomeSample(key=None, n_input=2, n_output=n_output,
+                                 node_genes=node_genes,
+                                 connection_genes=connection_genes)
+    return genome_sample
