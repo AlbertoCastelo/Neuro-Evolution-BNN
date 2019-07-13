a4c52a267cab86855e088f840c2ddba4f3512060
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2019-07-13 20:13:44 +0300

    remove unused code

--- neat/representation.py
+++ neat/representation.py
@@ -1,35 +1,7 @@
 import torch
 from torch import nn
-from torch.autograd import Variable
 
 from neat.genome import GenomeSample
-
-
-def _get_hidden_nodes(nodes: dict, n_output: int):
-    hidden_nodes = list(nodes.keys())[n_output:]
-    return hidden_nodes
-
-
-def get_nn_from_genome(genome: GenomeSample) -> nn.Module:
-    '''
-    Given a Genome, we get the Pytorch Neural Network model
-    '''
-    n_output = genome.n_output
-    n_input = genome.n_input
-    nodes = genome.node_genes
-    connections = genome.connection_genes
-
-    hidden_node_keys = _get_hidden_nodes(nodes, n_output)
-
-    layers = []
-    if hidden_node_keys:
-        pass
-    else:
-        layers.append(nn.Linear(n_input, n_output, bias=True))
-        layers.append(nn.Sigmoid())
-
-    network = Network(layers=layers)
-    return network
 
 
 class Network(nn.Module):
