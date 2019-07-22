97be07b8a3429c393102fd85767e83875181594d
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2019-07-22 19:33:51 +0300

    extend tests for stochastic network

--- tests/representation/test_stochastic_network.py
+++ tests/representation/test_stochastic_network.py
@@ -1,4 +1,4 @@
-from unittest import TestCase, skip
+from unittest import TestCase
 
 import torch
 from neat.representation.stochastic_network import StochasticNetwork
@@ -8,8 +8,7 @@ from tests.utils.generate_genome import generate_genome_with_hidden_units
 
 class TestStochasticFeedForwardWithoutHiddenLayers(TestCase):
 
-    # @skip('WIP')
-    def test_network_structure(self):
+    def test_network_structure_miso(self):
         self.config = create_configuration(filename='/miso.json')
         genome = generate_genome_with_hidden_units(n_input=self.config.n_input,
                                                    n_output=self.config.n_output)
@@ -20,3 +19,43 @@ class TestStochasticFeedForwardWithoutHiddenLayers(TestCase):
         result = model(input.data)
 
         self.assertEqual(len(result), self.config.n_output)
+
+    def test_network_structure_siso(self):
+        self.config = create_configuration(filename='/siso.json')
+        genome = generate_genome_with_hidden_units(n_input=self.config.n_input,
+                                                   n_output=self.config.n_output)
+
+        model = StochasticNetwork(genome=genome)
+
+        input = torch.tensor([1.0])
+        result = model(input.data)
+
+        self.assertEqual(len(result), self.config.n_output)
+
+    def test_network_structure_siso_with_batch(self):
+        self.config = create_configuration(filename='/siso.json')
+        genome = generate_genome_with_hidden_units(n_input=self.config.n_input,
+                                                   n_output=self.config.n_output)
+
+        model = StochasticNetwork(genome=genome)
+
+        input = torch.tensor([[1.0],
+                             [1.0],
+                             [1.0]])
+        result = model(input.data)
+
+        self.assertEqual(result.shape[1], self.config.n_output)
+        self.assertEqual(result.shape[0], 3)
+
+    def test_same_input_gives_different_result(self):
+        self.config = create_configuration(filename='/siso.json')
+        genome = generate_genome_with_hidden_units(n_input=self.config.n_input,
+                                                   n_output=self.config.n_output)
+
+        model = StochasticNetwork(genome=genome)
+
+        input = torch.tensor([[1.0]])
+        result_1 = model(input.data)
+        result_2 = model(input.data)
+
+        self.assertNotEqual(result_1.item(), result_2.item())
