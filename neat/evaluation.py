892bc8adb58feef344c8c71646aa665e0ff6b661
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2019-07-25 09:58:14 +0300

    fix evaluation of Stochastic Network

--- neat/evaluation.py
+++ neat/evaluation.py
@@ -48,6 +48,78 @@ class EvaluationEngine:
         network.eval()
 
         # calculate Data log-likelihood (p(y*|x*,D))
+        chunks_output = []
+        chunks_y_batch = []
+        for i in range(n_samples):
+            for x_batch, y_batch in self.data_loader:
+                x_batch = x_batch.reshape((-1, genome.n_input))
+                x_batch = x_batch.float()
+
+                y_batch = y_batch.float()
+                if is_gpu:
+                    x_batch, y_batch = x_batch.cuda(), y_batch.cuda()
+
+                with torch.no_grad():
+                    # forward pass
+                    output = network(x_batch)
+                    chunks_output.append(output)
+                    chunks_y_batch.append(y_batch)
+        output = torch.cat(chunks_output, dim=0)
+        y_batch = torch.cat(chunks_y_batch, dim=0)
+        kl_posterior = self.loss(y_pred=output, y_true=y_batch, kl_qw_pw=kl_qw_pw, beta=self.get_beta())
+        return kl_posterior.item()
+
+    def get_beta(self):
+
+        # if self.beta_type is 'Blundell':
+        #     beta = 2 ** (m - (batch_idx + 1)) / (2 ** m - 1)
+        # elif self.beta_type is 'Soenderby':
+        #     beta = min(epoch / (n_epochs // 4), 1)
+        # elif self.beta_type is 'Standard':
+        #     beta = 1 / m
+        # else:
+        #     beta = 0
+        beta = 1 / self.m
+        return beta
+
+
+class EvaluationStochasticGoodEngine:
+    def __init__(self, testing=False):
+        self.config = get_configuration()
+        self.batch_size = self.config.batch_size
+
+        self.dataset = get_dataset(self.config.dataset_name, testing=testing)
+        self.data_loader = DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True, num_workers=4)
+        self.m = math.ceil(len(self.data_loader) / self.batch_size)
+
+        self.loss = get_loss(problem_type=self.config.problem_type)
+
+    def evaluate(self, population: dict):
+        # TODO: make n_samples increase with generation number
+        n_samples = self.config.n_samples
+
+        for key, genome in population.items():
+            genome.fitness = self.evaluate_genome(genome=genome,
+                                                  n_samples=n_samples)
+
+        return population
+
+    def evaluate_genome(self, genome: Genome, n_samples=10, is_gpu=False):
+        '''
+        Calculates: KL-Div(q(w)||p(w|D))
+        Uses the VariationalInferenceLoss class (not the alternative)
+        '''
+        kl_posterior = 0
+
+        kl_qw_pw = compute_kl_qw_pw(genome=genome)
+
+        # setup network
+        network = StochasticNetwork(genome=genome)
+        if is_gpu:
+            network.cuda()
+        network.eval()
+
+        # calculate Data log-likelihood (p(y*|x*,D))
         for x_batch, y_batch in self.data_loader:
             x_batch = x_batch.reshape((-1, genome.n_input))
             x_batch = x_batch.view(-1, genome.n_input).repeat(n_samples, 1)
