f6da8c0a16d4e33c419260c24beb01791ea4c1aa
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2019-10-20 11:53:49 +0200

    add mutation of the architecture

--- neat/evolution_operators/mutation.py
+++ neat/evolution_operators/mutation.py
@@ -1,4 +1,9 @@
-from neat.configuration import get_configuration
+import copy
+import itertools
+import random
+
+from neat.configuration import get_configuration, BaseConfiguration
+from neat.gene import NodeGene, ConnectionGene
 from neat.genome import Genome
 
 
@@ -6,43 +11,21 @@ class Mutation:
 
     def __init__(self):
         self.config = get_configuration()
+
+        self.fix_architecture = self.config.fix_architecture
         self.single_structural_mutation = self.config.single_structural_mutation
         self.mutate_rate = self.config.mutate_rate
         self.mutate_power = self.config.mutate_power
         self.replace_rate = self.config.replace_rate
 
-        # self.node_add_prob = self.config.node_add_prob
-        # self.node_delete_prob = self.config.node_delete_prob
-        # self.conn_add_prob = self.config.conn_add_prob
-        # self.conn_delete_prob = self.config.conn_delete_prob
+        self.node_add_prob = self.config.node_add_prob
+        self.node_delete_prob = self.config.node_delete_prob
+        self.conn_add_prob = self.config.conn_add_prob
+        self.conn_delete_prob = self.config.conn_delete_prob
 
     def mutate(self, genome: Genome):
-        # if self.single_structural_mutation:
-        #     div = max(1, (self.node_add_prob + self.node_delete_prob +
-        #                   self.conn_add_prob + self.conn_delete_prob))
-        #     r = random.random()
-        #     if r < (self.node_add_prob/div):
-        #         genome = self.mutate_add_node(genome)
-        #     elif r < ((self.node_add_prob + self.node_delete_prob)/div):
-        #         genome = self.mutate_delete_node(genome)
-        #     elif r < ((self.node_add_prob + self.node_delete_prob +
-        #                self.conn_add_prob)/div):
-        #         genome = self.mutate_add_connection(genome)
-        #     elif r < ((self.node_add_prob + self.node_delete_prob +
-        #                self.conn_add_prob + self.conn_delete_prob)/div):
-        #         genome = self.mutate_delete_connection(genome)
-        # else:
-        #     if random.random() < self.node_add_prob:
-        #         genome = self.mutate_add_node(genome)
-        #
-        #     if random.random() < self.node_delete_prob:
-        #         genome = self.mutate_delete_node(genome)
-        #
-        #     if random.random() < self.conn_add_prob:
-        #         genome = self.mutate_add_connection(genome)
-        #
-        #     if random.random() < self.conn_delete_prob:
-        #         genome = self.mutate_delete_connection(genome)
+        if self.fix_architecture:
+            genome = self._mutate_architecture(genome)
 
         # Mutate connection genes.
         for key in genome.connection_genes.keys():
@@ -52,15 +35,247 @@ class Mutation:
             genome.node_genes[key].mutate()
         return genome
 
-    def mutate_add_node(self, genome):
-        raise
+    def _mutate_architecture(self, genome: Genome):
+        if self.single_structural_mutation:
+            div = max(1, (self.node_add_prob + self.node_delete_prob +
+                          self.conn_add_prob + self.conn_delete_prob))
+            r = random.random()
+            if r < (self.node_add_prob / div):
+                genome = self.mutate_add_node(genome)
+            elif r < ((self.node_add_prob + self.node_delete_prob) / div):
+                genome = self.mutate_delete_node(genome)
+            elif r < ((self.node_add_prob + self.node_delete_prob +
+                       self.conn_add_prob) / div):
+                genome = self.mutate_add_connection(genome)
+            elif r < ((self.node_add_prob + self.node_delete_prob +
+                       self.conn_add_prob + self.conn_delete_prob) / div):
+                genome = self.mutate_delete_connection(genome)
+        else:
+            if random.random() < self.node_add_prob:
+                genome = self.mutate_add_node(genome)
+
+            if random.random() < self.node_delete_prob:
+                genome = self.mutate_delete_node(genome)
+
+            if random.random() < self.conn_add_prob:
+                genome = self.mutate_add_connection(genome)
+
+            if random.random() < self.conn_delete_prob:
+                genome = self.mutate_delete_connection(genome)
         return genome
 
-    def mutate_delete_node(self, genome):
-        pass
+    def mutate_add_node(self, genome: Genome):
+        # if not genome.connection_genes:
+        #     # if config.check_structural_mutation_surer():
+        #     if self.is_valid_structural_mutation():
+        #         self.mutate_add_connection(config)
+        #     return
+        # Choose a random connection to split
+        conn_to_split = random.choice(list(genome.connection_genes.values()))
 
-    def mutate_add_connection(self, genome):
-        pass
+        new_node_key = genome.get_new_node_key()
+        new_node = NodeGene(key=new_node_key).random_initialization()
+        genome.node_genes[new_node_key] = new_node
+
+        # Disable this connection and create two new connections joining its nodes via
+        # the given node.  The new node+connections have roughly the same behavior as
+        # the original connection (depending on the activation function of the new node).
+        # TODO: handle enabled connection
+        conn_to_split.enabled = False
+
+        i, o = conn_to_split.key
+
+        # add connection between input and the new node
+        # TODO: careful I'm doing a random initialization but python-neat sets weight to 1
+        new_connection_key_i = (i, new_node_key)
+        new_connection_i = ConnectionGene(key=new_connection_key_i).random_initialization()
+        genome.connection_genes[new_connection_key_i] = new_connection_i
+        # genome.add_connection(config, i, new_node_key, 1.0, True)
+
+        # add connection between new node and output
+        new_connection_key_o = (new_node_key, o)
+        new_connection_o = ConnectionGene(key=new_connection_key_o).random_initialization()
+        new_connection_o.set_mean(mean=conn_to_split.get_mean())
+        new_connection_o.set_std(std=conn_to_split.get_std())
+        genome.connection_genes[new_connection_key_o] = new_connection_o
+        # self.add_connection(config, new_node_id, o, conn_to_split.weight, True)
+        return genome
+
+    def mutate_delete_node(self, genome: Genome):
+        # Do nothing if there are no non-output nodes.
+        available_nodes = [k for k in genome.node_genes.keys() if k not in self.config.n_output]
+        if not available_nodes:
+            return -1
+
+        del_key = random.choice(available_nodes)
+
+        connections_to_delete = set()
+        for k, v in genome.connection_genes.items():
+            if del_key in v.key:
+                connections_to_delete.add(v.key)
+
+        for key in connections_to_delete:
+            del genome.connection_genes[key]
+
+        del genome.node_genes[del_key]
+
+        return genome
+
+    def mutate_add_connection(self, genome: Genome):
+        possible_outputs = list(genome.node_genes.keys())
+        out_node = random.choice(possible_outputs)
+
+        possible_inputs = self._calculate_possible_inputs(genome,
+                                                          out_node_key=out_node.key,
+                                                          config=self.config)
+        if len(possible_inputs) == 0:
+            # TODO: Log
+            pass
+        in_node = random.choice(possible_inputs)
+
+        new_connection_key = (in_node, out_node)
+
+        # # Don't duplicate connections.
+        # if new_connection_key in genome.connection_genes:
+        #     # T#ODO: Should this be using mutation to/from rates? Hairy to configure...
+        #     # if config.check_structural_mutation_surer():
+        #     #     genome.connection_genes[key].enabled = True
+        #     return genome
+        #
+        # # Don't allow connections between two output nodes
+        # if in_node in genome.get_output_nodes_keys() and out_node in genome.get_output_nodes_keys():
+        #     return genome
+        #
+        # # For feed-forward networks, avoid creating cycles.
+        # hypothetical_connection_keys = list(genome.connection_genes.keys()) + [new_connection_key]
+        # if (self.config.feed_forward and contains_cycles(connection_keys=hypothetical_connection_keys) and
+        #         is_multi_hop(connection_keys=hypothetical_connection_keys)):
+        #     return genome
+
+        new_connection = ConnectionGene(key=new_connection_key).random_initialization()
+        genome.connection_genes[new_connection_key] = new_connection
+        return genome
 
     def mutate_delete_connection(self, genome: Genome):
-        pass
+        if genome.connection_genes:
+            key = random.choice(list(genome.connection_genes.keys()))
+            del genome.connection_genes[key]
+        return genome
+
+    @staticmethod
+    def _calculate_possible_inputs(genome: Genome, out_node_key: int, config: BaseConfiguration):
+        # all nodes
+        possible_input_keys_set = set(genome.node_genes.keys()).union(set(genome.get_input_nodes_keys()))
+
+        # no connection between two output nodes
+        possible_input_keys_set -= set(genome.get_output_nodes_keys())
+
+        if config.feed_forward:
+            # avoid self-recurrency
+            possible_input_keys_set -= {out_node_key}
+            # pass
+
+        # REMOVE POSSIBLE CONNECTIONS
+        possible_connection_set = set(itertools.product(list(possible_input_keys_set), [out_node_key]))
+
+        # remove already existing connections: don't duplicate connections
+        possible_connection_set -= set(genome.connection_genes.keys())
+
+        # remove possible connections that introduce cycles
+        possible_connection_set = \
+            Mutation._remove_connection_that_introduces_cycles(genome=genome,
+                                                               possible_connection_set=possible_connection_set)
+
+        # remove possible connections that introduce multihop jumps
+        possible_connection_set = \
+            Mutation._remove_connection_that_introduces_multihop_jumps(genome=genome,
+                                                                       possible_connection_set=possible_connection_set)
+        if len(possible_connection_set) == 0:
+            return []
+        possible_input_keys_set = list(zip(*list(possible_connection_set)))[0]
+        return possible_input_keys_set
+
+    @staticmethod
+    def _remove_connection_that_introduces_cycles(genome: Genome, possible_connection_set: set) -> set:
+        connections_to_remove = []
+        for connection in possible_connection_set:
+            connections = list(genome.connection_genes.keys()) + [connection]
+
+            if exist_cycle(connections=connections):
+                connections_to_remove.append(connection)
+        possible_connection_set -= set(connections_to_remove)
+        return possible_connection_set
+
+    @staticmethod
+    def _remove_connection_that_introduces_multihop_jumps(genome: Genome, possible_connection_set: set) -> set:
+        output_node_keys = genome.get_output_nodes_keys()
+        input_node_keys = genome.get_input_nodes_keys()
+        connections_to_remove = []
+        for connection in possible_connection_set:
+            connections = list(genome.connection_genes.keys()) + [connection]
+
+            if adds_multihop_jump(connections=connections,
+                                  output_node_keys=output_node_keys,
+                                  input_node_keys=input_node_keys):
+                connections_to_remove.append(connection)
+        possible_connection_set -= set(connections_to_remove)
+        return possible_connection_set
+
+
+def adds_multihop_jump(connections: list, output_node_keys, input_node_keys) -> bool:
+    layer_counter = 0
+    layers = {0: output_node_keys}
+
+    nodes = []
+    layer_output_node_keys = copy.deepcopy(output_node_keys)
+    is_not_done = True
+    while is_not_done:
+
+        nodes_in_previous_layer_set = set()
+        for connection in connections:
+            input_node = connection[0]
+            output_node = connection[1]
+            if output_node in layer_output_node_keys:
+                if input_node in nodes:
+                    return True
+                nodes_in_previous_layer_set = nodes_in_previous_layer_set.union({input_node})
+        nodes += list(nodes_in_previous_layer_set)
+
+        # keep
+        if len(nodes_in_previous_layer_set) == 0:
+            is_not_done = False
+        else:
+            layer_counter += 1
+            layers[layer_counter] = nodes_in_previous_layer_set
+            layer_output_node_keys = nodes_in_previous_layer_set
+
+    return False
+
+
+def exist_cycle(connections: list) -> bool:
+    # change data structure
+    con = {}
+    for connection in connections:
+        if connection[0] in con:
+            con[connection[0]].append(connection[1])
+        else:
+            con[connection[0]] = [connection[1]]
+
+    def _go_throgh_graph(node_in, graph, past=[]):
+        # print(f'{node_in}. {past}')
+        if node_in in graph.keys():
+            for node in graph[node_in]:
+                if node in past:
+                    return True
+                else:
+                    past_copy = copy.deepcopy(past)
+                    past_copy.append(node_in)
+                    if _go_throgh_graph(node_in=node, graph=graph, past=past_copy):
+                        return True
+        else:
+            return False
+
+    for node_in, nodes_out in con.items():
+        if _go_throgh_graph(node_in, graph=con, past=[]):
+            return True
+    return False
--- tests/test_mutation.py
+++ tests/test_mutation.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from neat.evolution_operators.mutation import Mutation, exist_cycle
+from neat.evolution_operators.mutation import Mutation, exist_cycle, adds_multihop_jump
 from tests.config_files.config_files import create_configuration
 from tests.utils.generate_genome import generate_genome_with_hidden_units
 
@@ -16,9 +16,57 @@ class TestMutation(TestCase):
         possible_inputs = list(Mutation._calculate_possible_inputs(genome=genome,
                                                                    out_node_key=out_node.key,
                                                                    config=self.config))
-        expected_possible_inputs = [1, 3]
-        self.assertEqual(possible_inputs, expected_possible_inputs)
+        expected_possible_inputs = []
+        self.assertEqual(expected_possible_inputs, possible_inputs)
 
+    def test_remove_connection_that_introduces_multihop_jumps(self):
+        self.config = create_configuration(filename='/miso.json')
+        genome = generate_genome_with_hidden_units(n_input=self.config.n_input,
+                                                   n_output=self.config.n_output,
+                                                   n_hidden=3)
+        possible_connection_set = {(1, 2), (2, 2), (2, 3)}
+
+        possible_connection_set = \
+            Mutation._remove_connection_that_introduces_multihop_jumps(genome=genome,
+                                                                       possible_connection_set=possible_connection_set)
+        expected_possible_connection_set = set()
+        self.assertSetEqual(possible_connection_set, expected_possible_connection_set)
+
+    def test_remove_connection_that_introduces_cycles(self):
+        self.config = create_configuration(filename='/miso.json')
+        genome = generate_genome_with_hidden_units(n_input=self.config.n_input,
+                                                   n_output=self.config.n_output,
+                                                   n_hidden=3)
+        possible_connection_set = {(1, 2), (2, 2), (2, 3)}
+
+        possible_connection_set = \
+            Mutation._remove_connection_that_introduces_cycles(genome=genome,
+                                                               possible_connection_set=possible_connection_set)
+        expected_possible_connection_set = {(1, 2), (2, 3)}
+        self.assertSetEqual(possible_connection_set, expected_possible_connection_set)
+
+
+class TestMultihopJumps(TestCase):
+    def test_adds_multihop_jump_true_a(self):
+        connections = [(-1, 1), (-1, 2), (-1, 3), (-2, 1), (-2, 2), (-2, 3), (1, 0), (2, 0), (3, 0), (1, 2)]
+        self.assertEqual(True, adds_multihop_jump(connections=connections,
+                                                  output_node_keys=[0],
+                                                  input_node_keys=[-1, -2]))
+
+    def test_adds_multihop_jump_true_b(self):
+        connections = [(-1, 1), (-2, 1), (1, 2), (2, 3), (2, 0), (3, 0)]
+        self.assertEqual(True, adds_multihop_jump(connections=connections,
+                                                  output_node_keys=[0],
+                                                  input_node_keys=[-1, -2]))
+
+    def test_adds_multihop_jump_false_a(self):
+        connections = [(-1, 1), (-1, 2), (-1, 3), (-2, 1), (-2, 2), (-2, 3), (1, 0), (2, 0), (3, 0)]
+        self.assertEqual(False, adds_multihop_jump(connections=connections,
+                                                   output_node_keys=[0],
+                                                   input_node_keys=[-1, -2]))
+
+
+class TestExistCycles(TestCase):
     def test_exists_cycle_positive_a(self):
         connections = [(1, 2), (2, 3), (3, 1)]
 
