1c7c3107f4736b80bca697deb64198954d52a0a5
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2019-10-29 12:58:31 +0100

    improve reporting

--- neat/reports.py
+++ neat/reports.py
@@ -27,7 +27,8 @@ class EvolutionReport(BaseReport):
                 'all_fitness': fitness_all,
                 'min': round(min(fitness_all), 3),
                 'max': round(max(fitness_all), 3),
-                'mean': round(np.mean(fitness_all), 3)}
+                'mean': round(np.mean(fitness_all), 3),
+                }
         logger.info(f'Generation {generation}. Best fitness: {round(max(fitness_all), 3)}. '
                      f'Mean fitness: {round(np.mean(fitness_all), 3)}')
         self.generation_metrics[generation] = data
@@ -35,6 +36,8 @@ class EvolutionReport(BaseReport):
         if self.best_individual is None or self.best_individual.fitness < best_individual_fitness:
             self.best_individual = population.get(best_individual_key)
             logger.debug(f'    New best individual found:{round(self.best_individual.fitness, 3)}')
+            logger.debug(f'         best individual has {len(self.best_individual.node_genes)} Nodes '
+                         f'and {len(self.best_individual.connection_genes)} Connections')
 
     def generate_final_report(self):
         best_individual = self.get_best_individual().to_dict()
