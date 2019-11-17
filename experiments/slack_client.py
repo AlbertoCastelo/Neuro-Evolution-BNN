4edeb986352e555b8bc117b31b2d13d4c65ce186
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2019-11-17 19:25:55 +0100

    notifier establishes connection each time it needs to send something

--- experiments/slack_client.py
+++ experiments/slack_client.py
@@ -1,5 +1,8 @@
 from slack import WebClient
 import os
+
+from experiments.logger import logger
+
 SLACK_TOKEN = 'SLACK_API_TOKEN'
 
 
@@ -13,21 +16,24 @@ class SlackNotifier(Notifier):
     def create(channel):
         # get it here: https://api.slack.com/apps/APKLV82ER/oauth?
         slack_token = os.getenv(SLACK_TOKEN)
-        return SlackNotifier(slack_client=WebClient(token=slack_token),
+        return SlackNotifier(slack_token=slack_token,
                              channel=channel)
 
-    def __init__(self, slack_client, channel):
-        self.slack_client = slack_client
+    def __init__(self, slack_token, channel):
+        self.slack_token = slack_token
         self.channel = channel
 
     def send(self, message):
-
-        result = self.slack_client.chat_postMessage(
-            channel=self.channel,
-            text=message
-        )
-        if not result['ok']:
-            # TODO log error
-            print(f'        SlackProducer failed to send message: {message}')
+        try:
+            slack_client = WebClient(token=self.slack_token)
+            result = slack_client.chat_postMessage(
+                channel=self.channel,
+                text=message
+            )
+            if not result['ok']:
+                # TODO log error
+                print(f'        SlackProducer failed to send message: {message}')
+        except Exception as e:
+            logger.exception(e)
 
         return result
