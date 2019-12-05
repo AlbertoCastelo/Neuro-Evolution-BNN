fe86c2acfc61383954913c34f46ff68a8ac6450e
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2019-12-05 11:58:33 +0100

    multiprocessing tests

---
+++ tests_non_automated/multiprocessing/pool_genome.py
@@ -0,0 +1,113 @@
+import math
+import os
+from multiprocessing.pool import Pool
+
+import torch
+
+from neat.evaluation import MyPool, _prepare_batch_data, get_dataset
+from neat.fitness.kl_divergence import compute_kl_qw_pw
+from neat.genome import Genome
+from neat.loss.vi_loss import get_loss, get_beta
+from neat.neat_logger import get_neat_logger
+from neat.representation_mapping.genome_to_network.complex_stochastic_network import ComplexStochasticNetwork
+from tests.config_files.config_files import create_configuration
+
+config = create_configuration(filename='/mnist_binary.json')
+LOGS_PATH = f'{os.getcwd()}/'
+logger = get_neat_logger(path=LOGS_PATH)
+
+# N_SAMPLES = 10
+N_PROCESSES = 16
+N_GENOMES = 100
+
+genomes = []
+for i in range(N_GENOMES):
+    genome = Genome(key=i)
+    genome.create_random_genome()
+    genomes.append(genome)
+
+
+def evaluate_genome_parallel(x):
+    return evaluate_genome(*x)
+
+
+def evaluate_genome(genome: Genome, loss, beta_type, problem_type,
+                    batch_size=10000, n_samples=10, is_gpu=False):
+    '''
+    Calculates: KL-Div(q(w)||p(w|D))
+    Uses the VariationalInferenceLoss class (not the alternative)
+    '''
+    dataset = get_dataset(genome.genome_config.dataset_name, testing=True)
+    dataset.generate_data()
+
+    kl_posterior = 0
+
+    kl_qw_pw = compute_kl_qw_pw(genome=genome)
+
+    # setup network
+    network = ComplexStochasticNetwork(genome=genome)
+    if is_gpu:
+        network.cuda()
+    m = math.ceil(len(dataset.x) / batch_size)
+
+    network.eval()
+
+    # calculate Data log-likelihood (p(y*|x*,D))
+    x_batch, y_batch = dataset.x, dataset.y
+    x_batch, y_batch = _prepare_batch_data(x_batch=x_batch,
+                                           y_batch=y_batch,
+                                           problem_type=problem_type,
+                                           is_gpu=is_gpu,
+                                           n_input=genome.n_input,
+                                           n_output=genome.n_output,
+                                           n_samples=n_samples)
+
+    if is_gpu:
+        x_batch, y_batch = x_batch.cuda(), y_batch.cuda()
+
+    with torch.no_grad():
+        # forward pass
+        output, _ = network(x_batch)
+        # print(self.config.beta_type)
+        beta = get_beta(beta_type=beta_type, m=m, batch_idx=0, epoch=1, n_epochs=1)
+        # print(f'Beta: {beta}')
+        kl_posterior += loss(y_pred=output, y_true=y_batch, kl_qw_pw=kl_qw_pw, beta=beta)
+
+    loss_value = kl_posterior.item()
+    return loss_value
+
+
+tasks = []
+
+
+
+pool = Pool(N_PROCESSES)
+for genome in genomes:
+    logger.debug(f'Genome {genome.key}: {genome.get_graph()}')
+
+    # x = torch.zeros(2, 784).float()
+    # y = torch.zeros(2).long()
+    # x=dataset.x.clone().detach()
+    # y=dataset.y.clone().detach()
+    loss = get_loss('classification')
+    beta_type = 'other'
+    problem_type = 'classification'
+    batch_size = 100000
+    n_samples = 20
+    is_gpu = False
+    x = (genome.copy(),
+         # x, y,
+         loss, beta_type, problem_type,
+         batch_size, n_samples, is_gpu)
+
+    tasks.append(x)
+
+# TODO: fix logging when using multiprocessing. Easy fix is to disable
+fitnesses = list(pool.imap(evaluate_genome_parallel, tasks, chunksize=max([len(genomes)//N_PROCESSES, 1])))
+
+pool.close()
+for i, genome in enumerate(genomes):
+    print(fitnesses[i])
+    genome.fitness = fitnesses[i]
+
+print('finished')
---
+++ tests_non_automated/multiprocessing/torch_multiprocessing_genome.py
@@ -0,0 +1,69 @@
+from multiprocessing import Manager
+import os
+import torch
+
+from experiments.multiprocessing_utils import Worker
+from neat.evaluation import Task
+from neat.genome import Genome
+from neat.loss.vi_loss import get_loss
+from neat.neat_logger import get_neat_logger
+from tests.config_files.config_files import create_configuration
+
+config = create_configuration(filename='/mnist_binary.json')
+LOGS_PATH = f'{os.getcwd()}/'
+logger = get_neat_logger(path=LOGS_PATH)
+
+# N_SAMPLES = 10
+N_PROCESSES = 16
+N_GENOMES = 5
+
+genomes = []
+for i in range(N_GENOMES):
+    genome = Genome(key=i)
+    genome.create_random_genome()
+    genomes.append(genome)
+
+
+manager = Manager()
+task_queue = manager.Queue()
+exit_queue = manager.Queue()
+exception_queue = manager.Queue()
+results_queue = manager.Queue()
+
+
+workers = []
+for i in range(N_PROCESSES):
+    worker = Worker(task_queue=task_queue,
+                    exit_queue=exit_queue,
+                    exception_queue=exception_queue,
+                    results_queue=results_queue)
+    worker.start()
+    workers.append(worker)
+
+for genome in genomes:
+    task_queue.put(Task(genome=genome.copy(), dataset=None,
+                        x=torch.zeros((2, 784)).float(),
+                        y=torch.zeros(2).long(),
+                        # x=self.dataset.x.clone().detach(),
+                        # y=self.dataset.y.clone().detach(),
+                        loss=get_loss('classification'),
+                        beta_type='other',
+                        problem_type='classification',
+                        batch_size=100000,
+                        n_samples=20,
+                        is_gpu=False))
+
+while not task_queue.empty():
+    print('reading results from workers')
+    print(task_queue.qsize())
+    # for worker in workers:
+    #     print(f'Is alive: {worker.is_alive()}')
+    # print('reading results from workers')
+    # print(task_queue.qsize())
+    if not exception_queue.empty():
+        exception = exception_queue.get()
+        raise exception
+    results = results_queue.get()
+    print(results)
+
+print('finished')
