94915f6dd128a189754d2533c72a67469185bab3
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2020-05-10 09:46:28 +0200

    create datasets with noisy attributes

--- neat/dataset/classification_titanic.py
+++ neat/dataset/classification_titanic.py
@@ -1,7 +1,7 @@
 import os
-
 import torch
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 from neat.dataset.abstract import NeatTestingDataset
@@ -13,21 +13,33 @@ class TitanicDataset(NeatTestingDataset):
     STD = [0.83766265,  0.48158299, 14.51632115,  0.92913212,  0.85269161, 52.88185844]
 
     def __init__(self, train_percentage, dataset_type='train', random_state=42, noise=0.0):
+        directory = os.path.dirname(os.path.realpath(__file__))
+        original_filename = ''.join([directory, f'/data/titanic/train.csv'])
+
+        filename_noisy = ''.join([directory, f'/data/titanic/train_{noise}.csv'])
+        if os.path.isfile(filename_noisy):
+            data = pd.read_csv(filename_noisy)
+        else:
+            # create noisy dataset and save it.
+            data_original = pd.read_csv(original_filename)
+            data = self._create_noisy_dataset(data_original, filename=filename_noisy, noise=noise)
+
+        self.data = data
         super().__init__(train_percentage=train_percentage, dataset_type=dataset_type, random_state=random_state,
                          noise=noise)
 
     def generate_data(self):
-        filename = ''.join([os.path.dirname(os.path.realpath(__file__)), '/data/titanic/train.csv'])
-        data = pd.read_csv(filename)
-        x = data.loc[:, self.COLUMNS_USED]
+        # filename = ''.join([os.path.dirname(os.path.realpath(__file__)), '/data/titanic/train.csv'])
+        # data = pd.read_csv(filename)
+        x = self.data.loc[:, self.COLUMNS_USED]
         x.dropna(axis=0, inplace=True)
-        x.loc[x['Sex'] == 'male', 'Sex'] = 0
-        x.loc[x['Sex'] == 'female', 'Sex'] = 1
+        # x.loc[x['Sex'] == 'male', 'Sex'] = 0
+        # x.loc[x['Sex'] == 'female', 'Sex'] = 1
         self.y = x['Survived'].values
         x.drop('Survived', axis=1, inplace=True)
         self.x_original = x.values
 
-        self.x_original = self._add_noise(x=self.x_original)
+        # self.x_original = self._add_noise(x=self.x_original)
 
         self.input_scaler = StandardScaler()
         self.input_scaler.fit(self.x_original)
@@ -40,3 +52,22 @@ class TitanicDataset(NeatTestingDataset):
 
     def __len__(self):
         return len(self.x)
+
+    def _create_noisy_dataset(self, data_original, filename, noise):
+        data_original = data_original.loc[:, self.COLUMNS_USED]
+        data_original.dropna(axis=0, inplace=True)
+        data_original.loc[data_original['Sex'] == 'male', 'Sex'] = 0
+        data_original.loc[data_original['Sex'] == 'female', 'Sex'] = 1
+        y = data_original['Survived'].values
+        x = data_original.drop(columns=['Survived'], axis=1)
+
+        means = np.mean(x, 0)
+        stds = np.std(x, 0)
+        x_noisy = ((x - means) / stds +
+                   np.random.normal(0, noise, size=x.shape)
+                   + means) * stds
+
+        data_noisy = pd.DataFrame(x_noisy, columns=data_original.columns[1:])
+        data_noisy['Survived'] = y
+        data_noisy.to_csv(filename, index=False)
+        return data_noisy
--- neat/dataset/iris.py
+++ neat/dataset/iris.py
@@ -3,6 +3,7 @@ import torch
 from sklearn.preprocessing import StandardScaler
 import os
 from neat.dataset.abstract import NeatTestingDataset
+import numpy as np
 
 
 class IrisDataset(NeatTestingDataset):
@@ -11,8 +12,19 @@ class IrisDataset(NeatTestingDataset):
     '''
 
     def __init__(self, train_percentage, dataset_type='train', random_state=42, noise=0.0, is_debug=False):
-        filename = ''.join([os.path.dirname(os.path.realpath(__file__)), '/data/iris/iris.csv'])
-        self.data = pd.read_csv(filename)
+        directory = os.path.dirname(os.path.realpath(__file__))
+        original_filename = ''.join([directory, f'/data/iris/iris.csv'])
+        if noise > 0:
+            filename_noisy = ''.join([directory, f'/data/iris/iris_{noise}.csv'])
+            if os.path.isfile(filename_noisy):
+                data = pd.read_csv(filename_noisy)
+            else:
+                # create noisy dataset and save it.
+                data_original = pd.read_csv(original_filename)
+                data = self._create_noisy_dataset(data_original, filename=filename_noisy, noise=noise)
+        else:
+            data = pd.read_csv(original_filename)
+        self.data = data
 
         super().__init__(train_percentage=train_percentage, dataset_type=dataset_type,
                          random_state=random_state, noise=noise)
@@ -22,7 +34,7 @@ class IrisDataset(NeatTestingDataset):
         df_x = self.data.iloc[:, :4]
         self.x_original = df_x.values
 
-        self.x_original = self._add_noise(x=self.x_original)
+        # self.x_original = self._add_noise(x=self.x_original)
 
         self.input_scaler = StandardScaler()
         self.input_scaler.fit(self.x_original)
@@ -41,3 +53,18 @@ class IrisDataset(NeatTestingDataset):
         x = self.x[idx]
         y = self.y[idx]
         return x, y
+
+    def _create_noisy_dataset(self, data, filename, noise):
+        y = data['target'].values
+        x = data.iloc[:, :4].values
+
+        means = np.mean(x, 0)
+        stds = np.std(x, 0)
+        x_noisy = ((x - means) / stds +
+                   np.random.normal(0, noise, size=x.shape)
+                   + means) * stds
+
+        data_noisy = pd.DataFrame(x_noisy, columns=data.columns[:4])
+        data_noisy['target'] = y
+        data_noisy.to_csv(filename, index=False)
+        return data_noisy
