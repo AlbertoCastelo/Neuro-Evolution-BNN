fee17c1bc8acc62fbb4ad6859c1a2e36becd3bf9
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2020-06-01 17:34:36 +0200

    add fixtures

--- tests/analysis/test_calibration_error.py
+++ tests/analysis/test_calibration_error.py
@@ -1,18 +1,14 @@
 from unittest import TestCase, skip
 import numpy as np
 from neat.analysis.uncertainty.calibration_error import expected_calibration_error
+import tests.fixtures.ece_fixtures as fixtures
 
 
 class TestCalibrationError(TestCase):
     def test_expected_calibration_error(self):
-        y_true = np.array([0, 1, 2, 0, 0, 1, 2])
-        y_pred_prob = np.array([[0.95, 0.01, 0.04],
-                                [0.05, 0.9, 0.05],
-                                [0.4, 0.2, 0.4],
-                                [0.36, 0.3, 0.34],
-                                [0.95, 0.01, 0.04],
-                                [0.95, 0.01, 0.04],
-                                [0.95, 0.01, 0.04]])
+        y_true = np.array(fixtures.y_true)
+
+        y_pred_prob = np.array(fixtures.y_pred_prob)
         ece, calibration_data = expected_calibration_error(y_true=y_true, y_pred_prob=y_pred_prob, n_bins=2)
 
-        self.assertAlmostEqual(ece, 0.187, places=2)
+        self.assertAlmostEqual(ece, 0.03028, places=2)
